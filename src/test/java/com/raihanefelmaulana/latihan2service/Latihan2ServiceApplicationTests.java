eddc14bccf0a58cd4bbd8303a279b3dda7d6041f
RaihanEfelMaulana <113502255+RaihanEfelMaulana@users.noreply.github.com>
2022-09-21 22:19:47 +0700

    Add files via upload

---
+++ src/test/java/com/raihanefelmaulana/latihan2service/Latihan2ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raihanefelmaulana.latihan2service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Latihan2ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
